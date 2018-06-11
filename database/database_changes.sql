d849cd9bb68ea84cfdf4759ca5adec0ab46710f7
sourav <sourav.dara@netwininfo.com>
2018-06-11 10:39:33 +0530

    database changes

--- database/database_changes.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE `tbl_course_master` ADD `course_front_image` VARCHAR(200) NULL AFTER `course_image`; 
-
-ALTER TABLE `tbl_program_course` ADD `program_course_description` TEXT NULL AFTER `program_course_name`;
-
-ALTER TABLE `tbl_program_course` CHANGE `program_course_status` `program_course_status` TINYINT(1) NOT NULL DEFAULT '1' COMMENT '1:Active & 0:Inactive';
-
-ALTER TABLE `tbl_junior_centre` ADD `centre_latitude` VARCHAR(100) NULL AFTER `centre_address`, ADD `centre_longitude` VARCHAR(100) NULL AFTER `centre_latitude`;
